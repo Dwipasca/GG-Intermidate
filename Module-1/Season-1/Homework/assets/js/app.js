e8fb4cf38471dc2cfe4d51e8b40baa8d6cbb9fa7
Dwi Pasca <dpnursanti@gmail.com>
2021-07-08 14:48:25 +0800

    Homework Meatup 1 from module 1 done

---
+++ Module-1/Season-1/Homework/assets/js/app.js
@@ -0,0 +1,9 @@
+console.log("app.js is active");
+
+// selection element
+let btnSubmit = document.getElementById("btn-submit");
+
+// do something if the button click
+btnSubmit.addEventListener("click", () => {
+  alert("holaa");
+});
